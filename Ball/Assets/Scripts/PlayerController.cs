f977f4fbb03af0a8707114e2101121bff65e9959
Colton <coltonparry@gmail.com>
2015-01-27 23:23:09 -0700

    adding games

---
+++ Ball/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVeritcal = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal,0,moveVeritcal);
+
+        rigidbody.AddForce(movement * speed * Time.deltaTime);
+    }
+
+}
